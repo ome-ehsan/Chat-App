f7c83bd81e9054a47663eade0830589280dee6b1
ome-ehsan <ome2ehsan001@gmail.com>
2025-01-29 14:49:45 +0000

    setup cors

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ import { authRouter } from "./routes/auth-routes.js";
 import { messageRouter } from "./routes/message-routes.js";
 import { connectDB } from "./libs/db-config.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config()
 
 const port = process.env.PORT || 8001;
@@ -11,6 +12,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin : 'http://localhost:5173',
+    credentials: true
+}));
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 
