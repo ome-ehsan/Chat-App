ef4aed8fa5669ac7f8db7e5570d6280903cacd25
ome-ehsan <ome2ehsan001@gmail.com>
2025-02-02 19:18:59 +0000

    modify message route name

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
     credentials: true
 }));
 app.use("/api/auth", authRouter);
-app.use("/api/message", messageRouter);
+app.use("/api/messages", messageRouter);
 
 
 app.listen(port, ()=>{
