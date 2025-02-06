db9934677dffc09fa2bf28c485fbc48d844a9b07
ome-ehsan <ome2ehsan001@gmail.com>
2025-02-06 14:02:07 +0600

    fix file path

--- server/src/app.js
+++ server/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static.path.join(__dirname, "../client/dist"));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
     app.get("*", (req,res)=>{
         res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
     });
